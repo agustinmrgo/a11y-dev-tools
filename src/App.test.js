f23eb9a8ee7d90ec720fefe8c26af08b918d9f11
Agustin Marengo <agustinmrgo@gmail.com>
2022-07-01 13:39:20 -0300

    remove comments

--- src/App.test.js
+++ src/App.test.js
@@ -10,16 +10,3 @@ test("should not have any accessibility violations", async () => {
   const results = await axe(container);
   expect(results).toHaveNoViolations();
 });
-
-// import { axe, toHaveNoViolations } from "jest-axe";
-
-// expect.extend(toHaveNoViolations);
-
-// it("should demonstrate this matcher`s usage", async () => {
-//   const render = () => '<img src="#"/>';
-
-//   // pass anything that outputs html to axe
-//   const html = render();
-
-//   expect(await axe(html)).toHaveNoViolations();
-// });
